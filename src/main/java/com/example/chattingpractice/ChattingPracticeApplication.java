d083ff5b3bad1ba6c3c27faf3429349eb385fe39
Hyun <ghkdgus29@naver.com>
2023-09-15 17:37:25 +0900

    Initial commit

---
+++ src/main/java/com/example/chattingpractice/ChattingPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.chattingpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChattingPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChattingPracticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/chattingpractice/ChattingPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chattingpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChattingPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
